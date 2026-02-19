7c21be7f1b3900ccb69573c17cdf850e718dc347
Kacper <plachetka.kacper@gmail.com>
2026-02-19 15:52:51 +0100

    Initial project setup

---
+++ CarrierApp/CarrierAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarrierAppApp.swift
+//  CarrierApp
+//
+//  Created by Kacper Płachetka on 19/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct CarrierAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarrierApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CarrierApp
+//
+//  Created by Kacper Płachetka on 19/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
